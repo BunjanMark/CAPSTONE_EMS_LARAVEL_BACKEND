d1bf96dd05d1d49a0ef8aa4bb53e9e04d613fbf2
Bunjan, Mark <108747105+BunjanMark@users.noreply.github.com>
2024-12-14 15:11:09 +0800

    vite changes'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <!-- Load styles and scripts with Vite -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
